8dde307ec486cf6d47b61b6c1946db59976ade59
SandiIceMessanger <Sandi@indochat.co.id>
2021-06-08 18:19:02 +0700

    add some link src

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ export default {
     ],
     scripts: [
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js' },
+      { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js' },
+      { src: 'https://cdn.jsdelivr.net/npm/simple-datatables@latest' },
     ],
     
   },
